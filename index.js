58826ca00f3123d501badf2be87ebd09fd2f4c35
Itay Finkelshtein <itayf9@gmail.com>
2022-07-18 15:50:15 +0300

    add rolling dice

--- index.js
+++ index.js
@@ -8,4 +8,33 @@ document.querySelector("button").addEventListener("click", function()
 
     document.querySelector(".firstDice").setAttribute("src", "images/Dice-"+randRoll1+"-b.svg");
     document.querySelector(".secondDice").setAttribute("src", "images/Dice-"+randRoll2+"-b.svg");
+
+    document.querySelector(".scoreResults").innerHTML= randRoll1+ " - " + randRoll2;
+
+    var randRollDeg1= Math.floor(Math.random()*100 +1);
+    var randRollDeg2= Math.floor(Math.random()*100 +1);
+
+    var firstDice= document.querySelector(".firstDice");
+    var secondDice= document.querySelector(".secondDice");
+
+    firstDice.setAttribute("style", "--rollDeg: " +randRollDeg1+ "deg");
+    secondDice.setAttribute("style", "--rollDeg2: " +randRollDeg2+ "deg");
+
+
+
+    if (firstDice.classList.contains("diceA"))
+    {
+        firstDice.classList.toggle("diceA");
+        secondDice.classList.toggle("diceB");
+    }
+    else
+    {
+        firstDice.classList.toggle("diceA");
+        secondDice.classList.toggle("diceB");
+    }
+    firstDice.classList.add("diceA");
+    secondDice.classList.add("diceB");
+
+
+
 })
